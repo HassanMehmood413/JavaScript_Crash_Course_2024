2ade7a1ee02aba72aa8e71e327d3e851837d420a
Hassan Mehmood <ihassan463m@gmail.com>
2024-06-03 09:12:25 +0500

    Create script.js

---
+++ Video 01/script.js
@@ -0,0 +1,25 @@
+// 7 primitives in javascript as "nn bb ss u"
+
+let a = 34;
+let b = null;
+let c = BigInt("4541")
+let d = true; //boolean
+let e = "Hassan";
+let f = Symbol("I am a symbol")
+let g = undefined;
+console.log(typeof a, typeof b, typeof c, typeof d,typeof e , typeof f, typeof g);
+console.log(a,b,c,d,e,f,g);
+
+
+//Non-primitives datatypes => object
+
+const hassan = {
+    "Hassan" : true,
+     age: false,
+    Univesity : "uaf"
+}
+console.log(hassan["Univesity"]);
+
+
+
+
