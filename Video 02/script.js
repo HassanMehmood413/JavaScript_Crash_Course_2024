171ada6b90a87eb8441a728817444e8842af1c5b
Hassan Mehmood <ihassan463m@gmail.com>
2024-06-04 11:24:35 +0500

    Add files via upload

---
+++ Video 02/script.js
@@ -0,0 +1,19 @@
+let a = 3;
+let b = 4;
+let c = 9;
+function average(x,y){
+    return x+y/2;
+    console.log("Here is your average");
+    
+}
+
+
+console.log("So here is the average of a and c" , average(a,c));
+
+
+// We can also write funciton as function using arrow
+
+const  name = () =>{
+    console.log("My name is hassan");
+    
+}
