f1e092c02e8ab47a93d8ba9e314b55f4ac253ed6
Hassan Mehmood <ihassan463m@gmail.com>
2024-06-05 16:35:05 +0500

    Add files via upload
    In this Video, I have learnt about different methods used in strings and how to use them with different loops

---
+++ Video 03/script.js
@@ -0,0 +1,19 @@
+let name = "Hassan";
+console.log(name.length);
+// Different methods used in string are 
+.trim // to remove white spaces
+.concat // to combine two stirngs
+.slice // to print only given from the stirng
+.replace // to replace a character or anything from the string
+
+
+//Some examples:
+let my = "Yakka";
+console.log(my.concat(" is the friend of " + name));
+
+
+// use for loop to print a string
+let nm = "hassan";
+for(let i = 0 ; i<nm.length ; i++){
+    console.log(nm[i]);
+}
